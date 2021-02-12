11a967a1deb1e825057b062674bc95a52c8e17ee
yocosaka <sakayoco55@gmail.com>
2021-02-12 17:55:24 +0900

    Remove extra file

--- src/scripts/mymodules/addToggleToMoreBtn.js
+++
@@ -1,9 +0,0 @@
-// const addToggleToMoreBtn = () => {
-//   const todoMoreBtns = document.querySelectorAll('.todo__morebtn');
-//   todoMoreBtn.addEventListener('click', () => {
-//     console.log('working');
-
-//   });
-// };
-
-// export default addToggleToMoreBtn;
